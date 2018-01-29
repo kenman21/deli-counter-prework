1a8bfed250d16f55e6abc26fff2e45e5580d53e1
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-29 04:08:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,7 @@ def now_serving(katz_deli)
   if katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
   else 
-    katz_deli.shift()
+    puts "Currently serving #{katz_deli.shift()}"
     katz_deli
   end 
 end 
