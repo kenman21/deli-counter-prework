a0f6e1791433346f251f5f27b44d6e4bf8fb2fff
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-29 03:45:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,14 @@
 # Write your code here.
 
+def line(katzdeli)
+  if katzdeli.size > 1 
+    line = "The line is currently:"
+    katzdeli.each do |person|
+      people = people + person
+    end 
+  else 
+    line = "The line is currently empty."
+  end 
+  line
+end 
+      
